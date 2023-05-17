949ce520d2e7b6564c135cba870d510698f22b41
ostapturash <oturash@kse.org.ua>
2023-05-17 23:22:41 +0300

    Big Integer

---
+++ Practice 6/Program.cs
@@ -0,0 +1,18 @@
+﻿public class BigInteger
+{
+    private int[] _numbers;
+
+    public BigInteger(string value)
+    {
+        int length = value.Length;
+        
+        _numbers = new int[length];
+        
+        for (int i = 0; i < length; i++)
+        {
+            int digit = value[length - 1 - i] - '0';
+            
+            _numbers[i] = digit;
+        }
+    }
+}
